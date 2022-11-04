fcffabfaa9627c447218a2386e73230f696bc059
AdityansyachUS <117457776+AdityansyachUS@users.noreply.github.com>
2022-11-04 20:53:28 +0700

    Add files via upload

---
+++ UTS_PBO_Adityansyach/src/no/pkg5/nomer5.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.pkg5;
+
+/**
+ *
+ * @author HP
+ */
+class nomer5 {
+
+    nomer5(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void getSaldo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void simpanUang(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void ambilUang(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
